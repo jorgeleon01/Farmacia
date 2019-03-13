1bd7fbacee9eba69ea333470ff6c5c6e7731ad95
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-13 10:40:56 -0600

    3-Farmacia_Realice migracion_de_los_modelos_para_crear_las_tablas

--- farmacia/myapp/models.py
+++ farmacia/myapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Categoria(models.Model):
-    nombrec = models.CharField(max_length=60)
+    nombre = models.CharField(max_length=60)
 
-class Producto(object):
-    categoria = models.Foreignkey(Categoria)
-    nombrep = models.CharField(max_length=60)
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=60)
