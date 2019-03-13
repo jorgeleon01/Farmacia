3f2c36601ee77a68494033240acb5906e67d309a
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-13 12:33:40 -0600

    7-Farmacia_Se_agrego_codigo_para_mejorar_la_representacion_de_categorias

--- farmacia/myapp/models.py
+++ farmacia/myapp/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=60)
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=60)
     precio = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre
