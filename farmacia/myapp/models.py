2e7d9df415f7fdf9d2dd5737191a488d30ff0a9f
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-18 13:53:22 -0600

    11-Farmacia - Se Cambio todo el codigo html para trabajar con una plantilla de bootstrap y se arreglo el codigo html de la plantilla para ajustalo a mi necesidad y se agrego paginacion para mostrar cierta cantidad de productos y no cargar tanto la pagina

--- farmacia/myapp/models.py
+++ farmacia/myapp/models.py
@@ -11,6 +11,8 @@ class Producto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=60)
     precio = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=100)
+    publicacion = models.DateField()
     imagen = models.ImageField(upload_to='photo',null=True)
     def __str__(self):
         return self.nombre
--- farmacia/myapp/views.py
+++ farmacia/myapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator
 from myapp.models import Producto
 
 # Create your views here.
 
 def index(request):
-	#return HttpResponse("index")
-	return render(request,'myapp/index.html',{'productos': Producto.objects.all()})
+    producto_list = Producto.objects.all()
+    paginator = Paginator(producto_list, 3)
+
+    page = request.GET.get('page')
+    productos = paginator.get_page(page)
+    return render(request,'myapp/index.html',{'productos': productos})
